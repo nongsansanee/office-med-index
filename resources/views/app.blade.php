5809688f0d975280e2f46cbe634d9a690d5212e5
nongsansanee <nongcomster@gmail.com>
2022-12-06 15:21:14 +0700

    add all file

---
+++ resources/views/app.blade.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+@include('font-theme')
+<body class="font-sarabun">
+    <div class="grid my-6 mx-2 lg:mx-4 place-items-center ">
+        <h1 class="text-3xl font-bold underline">
+            รวมลิ้งค์ระบบสารสนเทศสำนักงานภาควิชาอายุรศาสตร์
+        </h1>
+        <h1 class="text-2xl font-bold bg-green-300 mt-4 p-4 rounded-md ">
+            OFFICE-MED
+        </h1>
+    </div>
+    <div class=" w-full lg:flex justify-center ">
+        <div class="my-4 mx-2 lg:mx-4 ">
+            <a href="https://dev-sum.simedicine.org/" target="_blank">
+               
+                    <div class=" p-2 rounded-lg shadow-lg bg-purple-100 lg:w-64 lg:h-40 ">
+                        <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">ระบบพัสดุ</h5>
+                        <p class="text-gray-700 text-sm mb-4">
+                        สำหรับเจ้าหน้าคลังสาขา บันทึกตัดสต๊อกคลังวัสดุทางการแพทย์
+                        </p>
+                    
+                    </div>
+               
+            </a>
+        </div>
+
+        <div class="my-4 mx-2 lg:mx-4 ">
+            <a href="" target="_blank">
+              
+                    <div class=" p-2 rounded-lg shadow-lg bg-green-100  lg:w-64 lg:h-40">
+                        <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">ระบบจองห้องประชุม</h5>
+                        <p class="text-gray-700 text-sm mb-4">
+                        สำหรับบุคลากรทุกคน
+                        </p>
+                    
+                    </div>
+              
+            </a>
+        </div>
+
+        <div class="my-4 mx-2 lg:mx-4 ">
+            <a href="https://dev-nuc.simedicine.org/" target="_blank">
+              
+                    <div class=" p-2 rounded-lg shadow-lg bg-blue-100  lg:w-64 lg:h-40">
+                        <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">ระบบค้นหาเอกสารเก่า</h5>
+                        <p class="text-gray-700 text-sm mb-4">
+                        สำหรับหน่วยสารบรรณใช้งานเท่านั้น
+                        </p>
+                    
+                    </div>
+              
+            </a>
+        </div>
+       
+      
+    </div>
+    <div class="w-full lg:flex justify-center">
+        <div class="my-4 mx-2 lg:mx-4 ">
+            <a href="" target="_blank">
+              
+                    <div class=" p-2 rounded-lg shadow-lg bg-pink-100  lg:w-64 lg:h-40">
+                        <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">ระบบบัญชี</h5>
+                        <p class="text-gray-700 text-sm mb-4">
+                        สำหรับบันทึกข้อมูลเบิกจ่ายงบประมาณภาควิชาฯ
+                        </p>
+                    
+                    </div>
+            </a>
+        </div>
+        <div class="my-4 mx-2 lg:mx-4 ">
+            <a href="" target="_blank">
+              
+                    <div class=" p-2 rounded-lg shadow-lg bg-yellow-100  lg:w-64 lg:h-40">
+                        <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">ระบบบุคลากร</h5>
+                        <p class="text-gray-700 text-sm mb-4">
+                        สำหรับบันทึกข้อมูลบุคลากรภาควิชาฯ
+                        </p>
+                    
+                    </div>
+            </a>
+        </div>
+        <div class="my-4 mx-2 lg:mx-4 ">
+            <a href="" target="_blank">
+              
+                    <div class=" p-2 rounded-lg shadow-lg bg-cyan-100  lg:w-64 lg:h-40">
+                        <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">ระบบการลาและรายงานทาบบัตร</h5>
+                        <p class="text-gray-700 text-sm mb-4">
+                        สำหรับบุคลากรภาควิชาฯ บันทึกลา และตรวจสอบรายงานทาบบัตร
+                        </p>
+                    
+                    </div>
+            </a>
+        </div>
+    </div>
+  
+</body>
+</html>
